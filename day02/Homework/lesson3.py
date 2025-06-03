1488919954d4ab85441b7c9d52a5c0a94cb348d1
NickoPirtskhalava <pikosnikh@gmail.com>
2025-06-03 18:57:30 +0400

    first commit

---
+++ day02/Homework/lesson3.py
@@ -0,0 +1,33 @@
+# 1)
+score = 100
+print("მოთამაშის ქულა:" + str(score))
+
+# 2)
+health = 95
+print("მოთამაშის სიცოცხლე:" + str(health))
+
+# 3)
+attacking_points = 50
+defence_points = 30
+print("მოთამაშის თავდასხმის ქულები:" + str(attacking_points) + ", დაცვის ქულები:" + str(defence_points))
+
+# 4)
+math_score = 85
+english_score = 92
+science_score = 78
+chemistry_score = 88
+history_score = 75
+
+print("მათემატიკის ქულა:", math_score)
+print("ინგლისურის ქულა:", english_score)
+print("მეცნიერების ქულა:", science_score)
+print("ქიმიის ქულა:", chemistry_score)
+print("ისტორიის ქულა:", history_score)
+
+# 5)
+name = "ნიკოლოზ"
+surname = "ფირცხალავა"
+age = 12
+MyAlmostAge = 13
+
+print("გამარჯობა, მე მქვია " + name + " " + surname + ", და მე ვარ " + str(age) + " და ვხვდები " + str(MyAlmostAge) + " წლის")
---
+++ day03/Homework/lesson4.py
@@ -0,0 +1,39 @@
+# 1)
+name = input("შეიყვანეთ თქვენი სახელი: ")
+last_name = input("შეიყვანეთ თქვენი გვარი: ")
+age = input("შეიყვანეთ თქვენი წლოვანება: ")
+
+print(name + " " + last_name + " არის " + str(age) + " წლის")
+
+# 2)
+first = float(input("შეიყვანეთ პირველი რიცხვი: "))
+second = float(input("შეიყვანეთ მეორე რიცხვი: "))
+
+print("ჯამი: " + str(first + second))
+print("განსხვავება: " + str(first - second))
+print("გამრავლება: " + str(first * second))
+print("გაყოფა: " + str(first / second))
+
+# 3)
+user1 = input("enter your name: ")
+print(user1 + " is loving GOA")
+
+# 4)
+math = 20 + int("20")
+math2 = math - 10
+math3 = math2 * 2
+print(math3)
+
+# 5)
+academy = input("შეიყვანეთ თქვენი სასწავლებელი აკადემია: ")
+school = input("შეიყვანეთ თქვენი სასწავლებელი სკოლა: ")
+name_input = input("შეიყვანეთ თქვენი სახელი: ")
+last_name_input = input("შეიყვანეთ თქვენი გვარი: ")
+age_input = input("შეიყვანეთ თქვენი წლოვანება: ")
+
+print("\nთქვენი შეყვანილი მონაცემები:")
+print("აკადემია: " + academy)
+print("სკოლა: " + school)
+print("სახელი: " + name_input)
+print("გვარი: " + last_name_input)
+print("ასაკი: " + age_input)
