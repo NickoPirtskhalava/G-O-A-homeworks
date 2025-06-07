f11aca9fa86550d546409517f900b5a1985330cd
NickoPirtskhalava <pikosnikh@gmail.com>
2025-06-07 21:02:05 +0400

    fifth commit

---
+++ day05/Classwork/Classwork.py
@@ -0,0 +1,16 @@
+# 1)
+name = input("შეიყვანეთ თქვენი სახელი: ")
+surname = input("შეიყვანეთ თქვენი გვარი: ")
+
+print(name + surname)
+
+# 2)
+name1 = float(input("შეიყვანეთ პირველი რიცხვი (name1): "))
+name2 = float(input("შეიყვანეთ მეორე რიცხვი (name2): "))
+
+print("ჯამი (+): " + str(name1 + name2))
+print("განსხვავება (-): " + str(name1 - name2))
+print("მთელი გაყოფა (//): " + str(name1 // name2))
+print("გაყოფა (/): " + str(name1 / name2))
+print("გამრავლება (*): " + str(name1 * name2))
+print("ხარისხში აყვანა (**): " + str(name1 ** name2))
